059dbf66b5707d80eb47439f125be1aa51f0f4d5
Alepando <aocamporam@gmail.com>
2026-02-14 03:43:22 -0600

    Summarize comments in the core

--- core/src/org/spartan/core/SpartanCore.hpp
+++ core/src/org/spartan/core/SpartanCore.hpp
@@ -10,7 +10,6 @@ namespace org::spartan::core {
         /**
          * Logs a message to the console using C++26 std::print.
          * * @param message The string view to log.
-         * @contract Require(message.length() > 0) - Message must not be empty.
          */
         void log(std::string_view message) {
             std::println("[Spartan-Core] >> {}", message);
